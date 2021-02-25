0d67e245e7f65ee81c50ae91e81329d9fff4df64
IgorPV <igor.pereira.vieira@gmail.com>
2021-02-25 15:54:10 -0300

    correção do exercício 2 para numeros iguais

--- exerciciosAula2/exercicioDois.java
+++ exerciciosAula2/exercicioDois.java
@@ -16,28 +16,26 @@ public class exercicioDois {
 		System.out.println("Entre com o número 3: ");
 		num3 = leia.nextInt();
 
-		if (num1 > num2 && num1 > num3) {
-			if (num2 > num3) {
+		if (num1 > num2 && num1 >= num3) {
+			if (num2 >= num3) {
 				System.out.print(num3 + " " + num2 + " " + num1);
 			} else {
 				System.out.print(num2 + " " + num3 + " " + num1);
 			}
 
 		}
-		if (num2 > num1 && num2 > num3) {
-			if (num1 > num3) {
+		if (num2 >= num1 && num2 >= num3) {
+			if (num1 >= num3) {
 				System.out.print(num3 + " " + num1 + " " + num2);
 			} else {
 				System.out.print(num1 + " " + num3 + " " + num2);
 			}
-		} else if (num3 > num1 && num3 > num2) {
-			if (num2 > num1) {
+		} else if (num3 >= num1 && num3 >= num2) {
+			if (num2 >= num1) {
 				System.out.print(num1 + " " + num2 + " " + num3);
-			} else {
-				System.out.print(num1 + " " + num2 + " " + num3);
+			} else if(num3!=num1){
+				System.out.print(num2 + " " + num1 + " " + num3);
 			}
 		}
-
-	}
 
 }
