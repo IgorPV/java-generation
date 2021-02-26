2251fb3c43bbe412eb27e750bd7fe60ab65387c8
IgorPV <igor.pereira.vieira@gmail.com>
2021-02-26 16:38:30 -0300

    melhorando a resolução do exercício 4

--- exerciciosAula3/exercicioQuatro.java
+++ exerciciosAula3/exercicioQuatro.java
@@ -7,39 +7,51 @@ public class exercicioQuatro {
 		Scanner ler = new Scanner(System.in);
 		int idade, totcalm = 0, homagr = 0, munerv = 0, outcalm = 0, nerv40 = 0, calm18 = 0, i = 0, gen, temp;
 
-		while (i < 150) { //laço para o tamanho do conjunto de dados
+		while (i < 150) { // laço para o tamanho do conjunto de dados
 
 			System.out.println("entre com a idade: ");
 			idade = ler.nextInt();
+			while (idade < 1 || idade > 120) {
+				System.out.println("entre com a idade: ");
+				idade = ler.nextInt();
+			}
 			System.out.println("Entre com 1 para masculino, 2 para feminino, 3 para Outros");
 			gen = ler.nextInt();
+			while (gen < 1 || gen > 3) {
+				System.out.println("Entre com 1 para masculino, 2 para feminino, 3 para Outros");
+				gen = ler.nextInt();
+			}
 			System.out.println("Entre com 1 para calma, 2 para nervosa, 3 para agressiva");
 			temp = ler.nextInt();
-			if (temp == 1) { //condicional para verificar pessoas calmas
+			while (temp < 1 || temp > 3) {
+				System.out.println("Entre com 1 para calma, 2 para nervosa, 3 para agressiva");
+				temp = ler.nextInt();
+			}
+			if (temp == 1) { // condicional para verificar pessoas calmas
 				totcalm++;
 			}
-			if (idade >= 40 && temp == 2) { //contando maiores de 40 anos nervosos
+			if (idade >= 40 && temp == 2) { // contando maiores de 40 anos nervosos
 				nerv40++;
 			}
-			if (idade <= 18 && temp == 1) { //contando menores de 18 anos calmos
+			if (idade <= 18 && temp == 1) { // contando menores de 18 anos calmos
 				calm18++;
 			}
-			switch (gen) { //verificando as respostas para gênero
-			case 1: 
-				if (temp == 3) { //contando homens agressivos
+			switch (gen) { // verificando as respostas para gênero
+			case 1:
+				if (temp == 3) { // contando homens agressivos
 					homagr++;
 				}
 			case 2:
-				if (temp == 2) { //contando mulheres nervosas
+				if (temp == 2) { // contando mulheres nervosas
 					munerv++;
 				}
 			case 3:
-				if (temp == 1) { //contando outros calmos
+				if (temp == 1) { // contando outros calmos
 					outcalm++;
 				}
 			}
 
-			i++; //incremento para repetição do while
+			i++; // incremento para repetição do while
 
 		}
 
