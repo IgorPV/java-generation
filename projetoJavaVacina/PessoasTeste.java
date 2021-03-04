cbb2e99c53e061bffc24a487e21a9ff807ce7825
IgorPV <igor.pereira.vieira@gmail.com>
2021-03-04 15:20:58 -0300

    Modificados métodos, e PessoasTeste realiza agora a leitura de dados

--- projetoJavaVacina/PessoasTeste.java
+++ projetoJavaVacina/PessoasTeste.java
@@ -4,17 +4,18 @@ public class PessoasTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Pessoas teste = new Pessoas("Igor","pereira","vieira","ZS","23/10/1993","1993",27);
-		Pessoas teste2 = new Pessoas(null, null, null, null, null, null, 0);
+		Pessoas teste[] = new Pessoas[1];
 		
 		
-		teste.setSenha();
+		for(int i=0; i<teste.length ;i++) {
+			teste[i] = new Pessoas(null,null,null,null,null,null,0);
+			teste[i].cadastrarPessoa();
+			teste[i].verificaCad();
+			teste[i].setStatusVacina();
+		}
 		
-		teste.verificaCad();
-		teste2.verificaCad();
 		
-		
-		
+			
 		
 	}
 
